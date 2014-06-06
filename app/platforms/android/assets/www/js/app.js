bb7208d657f9bf5ecbb643eeb532b1b0eda64207
luis <lrosario101@gmail.com>
2014-06-06 16:40:27 -0400

    after many fails at first attempt of routing we got something working again

--- app/platforms/android/assets/www/js/app.js
+++ app/platforms/android/assets/www/js/app.js
@@ -1,6 +1,6 @@
 (function () {
   /*
-    We use an "Immediate Function" 
+    We use an 'Immediate Function' 
     to initialize the application 
     to avoid leaving anything behind 
     in the global scope
@@ -8,96 +8,155 @@
 
 
   // LOCAL VARIABLES
-  // ----------------------------
+  // ===================================
 
-  // templates
+
+  var DOM = $('#main'); // where we actually append/remove
+  // var slider = new PageSlider($('body'));
+
+
+
+  // Templates
+  // ----------------------------
   var templates = {
     page: {
-      index: Handlebars.compile($("#page-index-tpl").html())
+      index: Handlebars.compile($('#page-index-tpl').html()),
+      assignment: Handlebars.compile($('#page-assignment-tpl').html())
     },
-    assignmentLI: Handlebars.compile($("#assignmentLI-tpl").html())
+    assignmentLI: Handlebars.compile($('#assignmentLI-tpl').html()),
+    contributionLI: Handlebars.compile($('#contributionLI-tpl').html())
   };
 
 
+
+  // URLs
+  // ----------------------------
   var urls = {
-    assignment: /^#assignment\/(\d{1,})/
+    assignment: /^#assignment\/(\d{1,})/,
+    contribution: /^#contribution\/(\d{1,})/,
+    report: /^#report\/(\d{1,})/
   };
 
-  // var slider = new PageSlider($('body'));
+
+
+  // Views
+  // ----------------------------
+  var views = {
+    index: function() {
+      // render base html
+      $('#main').html(templates.page.index());
+
+      // add <li> elements
+      var json = JSON.parse(window.localStorage.getItem('assignments'));
+      $('#latest-assignments').html(templates.assignmentLI(json.assignments));
+    },
+
+    // assignment view
+    assignment: function(id) {
+      var json = JSON.parse(window.localStorage.getItem('assignments'));
+
+      // find the right assignment
+      var assignment;
+      for (var i = 0; i < json.length; i++) {
+        if (json[i].id == id) {
+          assignment = json[i];
+          return;
+        }
+      };
+
+      DOM.html(templates.page.assignment(assignment));
+    }
+  };
+
+
+
+
+
+
+
 
 
   // add sample data
   $.ajax({
     url: 'data/assignments.json',
     beforeSend: function( xhr ) {
-      xhr.overrideMimeType( "text/plain; charset=x-user-defined" );
+      xhr.overrideMimeType( 'text/plain; charset=x-user-defined' );
     }
   })
   .done(function(data) {
     window.localStorage.setItem('assignments', data);
-    // var json = $.parseJSON(data);
-    // $('#latest-assignments').html(templates.assignmentLI(json.assignments));
-  });
-
-  var adapter = new LocalStorageAdapter();
-  adapter.initialize().done(function () {
-    // alert('yo');
   });
 
 
-  /* --------------------------------- Event Registration -------------------------------- */
+  // var adapter = new LocalStorageAdapter();
+  // adapter.initialize().done(function () {
+  //   // alert('yo');
+  // });
+
+
+
+
+
+
+  // EVENTS
+  // ----------------------------
   document.addEventListener('deviceready', function () {
 
+    // ios: remove delay for clicks
     FastClick.attach(document.body);
 
+    // user system notification boxes
     if (navigator.notification) { // Override default HTML alert with native dialog
       window.alert = function (message) {
         navigator.notification.alert(
           message,    // message
           null,       // callback
-          "Yellr",    // title
+          'Yellr',    // title
           'OK'        // buttonName
         );
       };
     }
 
-    index();
+
+    // load up the home page by default
+    route();
   }, false);
 
   $(window).on('hashchange', route);
 
-  /* ---------------------------------- Local Functions ---------------------------------- */
 
 
-  function index() {
-    // render base html
-    $('#main').html(
-      templates.page.index()
-    );
-
-    var json = JSON.parse(window.localStorage.getItem('assignments'));
-    // $.parseJSON(data);
-    $('#latest-assignments').html(templates.assignmentLI(json.assignments));
-
-    alert('did everything work?..');
-  }
 
 
+
+  // LOCAL FUNCTIONS
+  // ----------------------------
 
   function route() {
     var hash = window.location.hash;
+
     if (!hash) {
-      console.log('render home screen');
-      // slider.slidePage(new HomeView(adapter, homeTpl, employeeLiTpl).render().el);
+      // index();
+      views.index();
       return;
     }
-    var match = hash.match(detailsURL);
-    if (match) {
-      console.log('render new screen');
-      // adapter.findById(Number(match[1])).done(function(employee) {
-      //    // slider.slidePage(new EmployeeView(adapter, employeeTpl, employee).render().el);
-      // });
+    var match = hash.match(urls.assignment);
+
+    if (hash.match(urls.assignment)) {
+      // alert('view assignment');
+      alert(hash.split('/')[1]);
+      views.assignment(hash.split('/')[1]);
+    }
+
+    if (hash.match(urls.report)) {
+      alert('submit report');
+    }
+
+    if (hash.match(urls.contribution)) {
+      alert('view contribution');
     }
   }
+
+
 
 }());
--- app/platforms/ios/www/js/app.js
+++ app/platforms/ios/www/js/app.js
@@ -1,92 +1,162 @@
-// We use an "Immediate Function" to initialize the application to avoid leaving anything behind in the global scope
 (function () {
+  /*
+    We use an 'Immediate Function' 
+    to initialize the application 
+    to avoid leaving anything behind 
+    in the global scope
+  */
+
 
   // LOCAL VARIABLES
-  // ----------------------------
-
-  // templates
-  var templates = {
-    page: {
-      index: Handlebars.compile($("#page-index-tpl").html())
-    },
-    assignmentLI: Handlebars.compile($("#assignmentLI-tpl").html())
-  };
+  // ===================================
 
 
-  var urls = {
-    assignment: /^#assignment\/(\d{1,})/
-  };
-
+  var DOM = $('#main'); // where we actually append/remove
   // var slider = new PageSlider($('body'));
 
-  var adapter = new LocalStorageAdapter();
-  adapter.initialize().done(function () {
-    // alert('yo');
+
+
+  // Templates
+  // ----------------------------
+  var templates = {
+    page: {
+      index: Handlebars.compile($('#page-index-tpl').html()),
+      assignment: Handlebars.compile($('#page-assignment-tpl').html())
+    },
+    assignmentLI: Handlebars.compile($('#assignmentLI-tpl').html()),
+    contributionLI: Handlebars.compile($('#contributionLI-tpl').html())
+  };
+
+
+
+  // URLs
+  // ----------------------------
+  var urls = {
+    assignment: /^#assignment\/(\d{1,})/,
+    contribution: /^#contribution\/(\d{1,})/,
+    report: /^#report\/(\d{1,})/
+  };
+
+
+
+  // Views
+  // ----------------------------
+  var views = {
+    index: function() {
+      // render base html
+      $('#main').html(templates.page.index());
+
+      // add <li> elements
+      var json = JSON.parse(window.localStorage.getItem('assignments'));
+      $('#latest-assignments').html(templates.assignmentLI(json.assignments));
+    },
+
+    // assignment view
+    assignment: function(id) {
+      var json = JSON.parse(window.localStorage.getItem('assignments'));
+
+      // find the right assignment
+      var assignment;
+      for (var i = 0; i < json.length; i++) {
+        if (json[i].id == id) {
+          assignment = json[i];
+          return;
+        }
+      };
+
+      DOM.html(templates.page.assignment(assignment));
+    }
+  };
+
+
+
+
+
+
+
+
+
+  // add sample data
+  $.ajax({
+    url: 'data/assignments.json',
+    beforeSend: function( xhr ) {
+      xhr.overrideMimeType( 'text/plain; charset=x-user-defined' );
+    }
+  })
+  .done(function(data) {
+    window.localStorage.setItem('assignments', data);
   });
 
 
+  // var adapter = new LocalStorageAdapter();
+  // adapter.initialize().done(function () {
+  //   // alert('yo');
+  // });
 
-  /* --------------------------------- Event Registration -------------------------------- */
+
+
+
+
+
+  // EVENTS
+  // ----------------------------
   document.addEventListener('deviceready', function () {
 
+    // ios: remove delay for clicks
     FastClick.attach(document.body);
 
+    // user system notification boxes
     if (navigator.notification) { // Override default HTML alert with native dialog
       window.alert = function (message) {
         navigator.notification.alert(
           message,    // message
           null,       // callback
-          "Yellr",    // title
+          'Yellr',    // title
           'OK'        // buttonName
         );
       };
     }
 
-    index();
+
+    // load up the home page by default
+    route();
   }, false);
 
   $(window).on('hashchange', route);
 
-  /* ---------------------------------- Local Functions ---------------------------------- */
 
 
-  function index() {
-    // render base html
-    $('#main').html(
-      templates.page.index()
-    );
-
-    // add sample data
-    $.ajax({
-      url: 'data/assignments.json',
-      beforeSend: function( xhr ) {
-        xhr.overrideMimeType( "text/plain; charset=x-user-defined" );
-      }
-    })
-    .done(function( data ) {
-      // alert('assignments.json loaded');
-      var json = $.parseJSON(data);
-      // alert(json.assignments[0].title);
-      $('#latest-assignments').html(templates.assignmentLI(json.assignments));
-    });
-  }
 
 
+
+  // LOCAL FUNCTIONS
+  // ----------------------------
 
   function route() {
     var hash = window.location.hash;
+
     if (!hash) {
-      console.log('render home screen');
-      // slider.slidePage(new HomeView(adapter, homeTpl, employeeLiTpl).render().el);
+      // index();
+      views.index();
       return;
     }
-    var match = hash.match(detailsURL);
-    if (match) {
-      console.log('render new screen');
-      // adapter.findById(Number(match[1])).done(function(employee) {
-      //    // slider.slidePage(new EmployeeView(adapter, employeeTpl, employee).render().el);
-      // });
+    var match = hash.match(urls.assignment);
+
+    if (hash.match(urls.assignment)) {
+      // alert('view assignment');
+      alert(hash.split('/')[1]);
+      views.assignment(hash.split('/')[1]);
+    }
+
+    if (hash.match(urls.report)) {
+      alert('submit report');
+    }
+
+    if (hash.match(urls.contribution)) {
+      alert('view contribution');
     }
   }
+
+
 
 }());

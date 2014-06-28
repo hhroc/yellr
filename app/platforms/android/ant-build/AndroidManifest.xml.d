0d2307918feaabb9383021ea80d0b7aea69e2135
luis <lrosario101@gmail.com>
2014-06-28 13:47:53 -0400

    test build, make sure things are still working. checkin before develop branch checkout

--- app/platforms/android/ant-build/AndroidManifest.xml.d
+++ app/platforms/android/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/Users/sumo/Desktop/wxxi/yellr-repo/app/platforms/android/ant-build/AndroidManifest.xml : \
-/Users/sumo/Desktop/wxxi/yellr-repo/app/platforms/android/AndroidManifest.xml \
+/Users/sumo/Desktop/yellr-repo/app/platforms/android/ant-build/AndroidManifest.xml : \
+/Users/sumo/Desktop/yellr-repo/app/platforms/android/AndroidManifest.xml \

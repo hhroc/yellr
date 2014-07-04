0ec5f1e14f692f7144c2d1adbafbd64f08490ce8
luis <lrosario101@gmail.com>
2014-07-04 04:09:45 -0400

    new makefile 'make android'. weirdness going on with the css on my phone

--- src/app/js/init.js
+++ src/app/js/init.js
@@ -1,14 +1,18 @@
 'use strict';
 var yellr = yellr || {};
 
+/*
+	We call our setup functions as soon as we're ready
+	They are fired in the following sequence:
+		- onload()
+		- deviceready()
+*/
 
 window.onload = function() {
 	console.log('hello from: yellr 2.0');
-	yellr.init.run();
+	yellr.setup.DOM();
 }
 
 document.addEventListener('deviceready', function() {
-	alert('hello from: yellr deviceready');
-	yellr.init.app_test();
-
+	yellr.setup.app();
 }, false);
--- src/app/js/yellr.js
+++ src/app/js/yellr.js
@@ -1,11 +1,78 @@
 'use strict';
 var yellr = yellr || {};
 
-yellr.init = {
-	run: function() {
-		console.log('hello from: yellr.init.run');
+
+/*
+	the Yellr js objects is made up of:
+	yeller = {
+		setup: {
+			// which handles basic setup for the DOM and app
+		},
+		toggle: {
+			// to handle toggling things
+		}
+	}
+*/
+
+
+
+yellr.setup = {
+	DOM: function() {
+		/*
+			set up the DOM
+			mostly cosmetic things.. setting classes to things and such
+		*/
+		console.log('setup DOM');
+
+		// alert(Modernizr);
+		// check if CSS Flexbox is supported
+		Modernizr.load({
+			test: Modernizr.flexbox,
+			nope: ['test.js', 'js/test.js', 'js/libs/flexie.js'],
+			complete: function() {
+				yellr.fallback.flexbox();
+			}
+		});
+
+
+		// hide More options list
+		var doc = document;
+		var toggle = document.querySelector('#more-btn .toggle');
+		toggle.onclick = yellr.toggle.more_options;
+		yellr.toggle.more_options();
+
+		// var moreList = doc.querySelector('.more-options-list');
+		// if (moreList.getAttribute('data-hidden')) {
+		// 	moreList.className += ' hidden';
+		// }
+
+
 	},
-	app_test: function() {
-		console.log('hello from: app_test');
+	app: function() {
+		console.log('setup app');
 	}
 };
+
+
+
+
+yellr.toggle = {
+	more_options: function(e) {
+		var moreList = document.querySelector('.more-options-list');
+		if (moreList.getAttribute('data-hidden')) {
+			var c = moreList.className.split(' hidden')[0];
+			moreList.className = c;
+		} else {
+			moreList.className += ' hidden';
+			moreList.setAttribute('data-hidden', true);
+		}
+
+	}
+}
+
+yellr.fallback = {
+	flexbox: function() {
+		// fall back for flexbox
+		alert('do fallback for flexbox');
+	}
+}

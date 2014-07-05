97730e7f7f0d4b297a7d45ba8ddda95f2d0edbd7
luis <lrosario101@gmail.com>
2014-07-04 23:48:36 -0400

    new footer stuff ready

--- src/app/js/yellr.js
+++ src/app/js/yellr.js
@@ -29,6 +29,10 @@ yellr.setup = {
 		var toggle = document.querySelector('#more-btn .toggle');
 		toggle.onclick = yellr.toggle.more_options;
 
+
+		// set up toggle for content submission
+		document.querySelector('#submit-footer .flex').onclick = yellr.toggle.report_details;
+
 	},
 	app: function() {
 		console.log('setup app');
@@ -49,6 +53,44 @@ yellr.toggle = {
 		} else {
 			moreList.className += ' hidden';
 			moreList.setAttribute('data-hidden', 'true');
+		}
+	},
+	report_details: function(e) {
+		// the element holding the extra info
+		var container = document.querySelector('#submit-footer .more-info');
+		// current info showin
+		var current = container.getAttribute('data-current');
+		// what was just selected
+		var selected = e.target.getAttribute('data-info') || e.target.parentNode.getAttribute('data-info');
+		console.log(current, selected);
+
+		// show the thing
+		if (current === 'none') {
+			container.className = container.className.split(' hidden')[0];
+		}
+
+
+		var target;
+		var extras = document.querySelectorAll('.extra-info');
+		for (var i = 0; i < extras.length; i++) {
+			// clear selected classname
+			extras[i].className = extras[i].className.split('selected')[0];
+			// find our target
+			if (extras[i].getAttribute('data-info') == selected)
+				target = extras[i];
+		}
+		console.log(target);
+		target.className += ' selected';
+		container.setAttribute('data-current', selected);
+
+		// if (selected === current) {
+		// 	container.className += ' hidden';
+		// }
+
+		// close it if we have a toggle
+		if (selected === current) {
+			container.className += ' hidden';
+			container.setAttribute('data-current', 'none');
 		}
 
 	}

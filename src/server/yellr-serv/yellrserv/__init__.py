1710c228f562389b4bf6d4fd0113d61b4aaa62ff
thequbit <thequbit@gmail.com>
2014-07-22 14:01:54 -0400

    added new views

--- src/server/yellr-serv/yellrserv/__init__.py
+++ src/server/yellr-serv/yellrserv/__init__.py
@@ -1,4 +1,5 @@
 from pyramid.config import Configurator
+from pyramid.renderers import JSONP
 from sqlalchemy import engine_from_config
 
 from .models import (
@@ -15,15 +16,19 @@ def main(global_config, **settings):
     Base.metadata.bind = engine
     config = Configurator(settings=settings)
     config.include('pyramid_chameleon')
+
+    config.add_renderer('jsonp', JSONP(param_name='callback'))
+    config.add_route('json_test', 'json_test')
+
     config.add_static_view('static', 'static', cache_max_age=3600)
 
     config.add_route('index.html', '/')
     config.add_route('status.json','status.json')
-    config.add_route('clientlogs.json','clientlogs.json')  
-    config.add_route('getmessages.json','getmessages.json')
-    config.add_route('uploadmedia.json','uploadmedia.json')
-    config.add_route('uploadtest.json','uploadtest.json')
-    config.add_route('publishpost.json','publishpost.json')
+    config.add_route('client_logs.json','client_logs.json')  
+    config.add_route('get_messages.json','get_messages.json')
+    config.add_route('upload_media.json','upload_media.json')
+    config.add_route('upload_test.json','upload_test.json')
+    config.add_route('publish_post.json','publish_post.json')
     #config.add_route('','')
 
     config.scan()

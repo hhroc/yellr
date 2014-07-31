e56e7703e4f468c790782cf11b5cd3cf36ccf718
luis <lrosario101@gmail.com>
2014-07-31 16:04:46 -0400

    back to the jquery game. first run at uploading images

--- src/app/js/yellr/setup.js
+++ src/app/js/yellr/setup.js
@@ -64,7 +64,6 @@ yellr.setup = {
       console.log(
         'target: ', e.target,
         'parent: ', e.target.parentNode
-
         );
     })
 
@@ -103,6 +102,111 @@ yellr.setup = {
       }
       forms[i].querySelector('.client_id').value = yellr.localStorage.client_id;
     }
+
+
+
+    // alert('form setup');
+    $('#imageUploadForm').on('submit', function(e) {
+      e.preventDefault();
+
+      // alert('submitting...');
+      var formData = new FormData(this);
+      console.log(formData);
+      // alert(formData);
+
+      $.ajax({
+        type:'POST',
+        url: $(this).attr('action'),
+        data:formData,
+        cache:false,
+        contentType: false,
+        processData: false,
+        success:function(data){
+          console.log("success");
+          console.log(data);
+          // alert('you are winner ahaha');
+        },
+        error: function(data){
+          console.log("error");
+          console.log(data);
+          // alert('YOU LOSE');
+        }
+      });
+    });
+
+    // $("#ImageBrowse").on("change", function() {
+    //     $("#imageUploadForm").submit();
+    // });
+
+
+    // // Initialize the jQuery File Upload plugin
+    // var $form = $('#form-wrapper form');
+    // // $('#upload').fileupload({
+    // $form.fileupload({
+
+    //   // This element will accept file drag/drop uploading
+    //   dropZone: $('#form-wrapper'),
+
+    //   // This function is called when a file is added to the queue;
+    //   // either via the browse button, or via drag/drop:
+    //   add: function (e, data) {
+
+    //     // var tpl = $('<li class="working"><input type="text" value="0" data-width="48" data-height="48"'+
+    //     //     ' data-fgColor="#0788a5" data-readOnly="1" data-bgColor="#3e4043" /><p></p><span></span></li>');
+
+    //     // // Append the file name and file size
+    //     // tpl.find('p').text(data.files[0].name)
+    //     //              .append('<i>' + formatFileSize(data.files[0].size) + '</i>');
+
+    //     // // Add the HTML to the UL element
+    //     // data.context = tpl.appendTo(ul);
+
+    //     // // Initialize the knob plugin
+    //     // tpl.find('input').knob();
+
+    //     // // Listen for clicks on the cancel icon
+    //     // tpl.find('span').click(function(){
+
+    //     //   if(tpl.hasClass('working')){
+    //     //     jqXHR.abort();
+    //     //   }
+
+    //     //   tpl.fadeOut(function(){
+    //     //     tpl.remove();
+    //     //   });
+
+    //     // });
+
+    //     console.log('added new file..');
+    //     // Automatically upload the file once it is added to the queue
+    //     var jqXHR = data.submit();
+    //   },
+
+    //   progress: function(e, data){
+
+    //     // Calculate the completion percentage of the upload
+    //     var progress = parseInt(data.loaded / data.total * 100, 10);
+    //     console.log(progress);
+
+    //     // // Update the hidden input field and trigger a change
+    //     // // so that the jQuery knob plugin knows to update the dial
+    //     // data.context.find('input').val(progress).change();
+
+    //     // if(progress == 100){
+    //     //   data.context.removeClass('working');
+    //     // }
+    //   },
+
+    //   fail:function(e, data){
+    //     // Something has gone wrong!
+    //     alert('something went wrong with the file upload');
+    //     // data.context.addClass('error');
+    //   }
+    // });
+
+
+
+
 
   },
 

76fd556a71a8dba1103e1043cd73d734b424bca6
luis <lrosario101@gmail.com>
2014-09-16 12:12:22 -0400

    assignments index page more fleshed out

--- src/moderator/js/setup.js
+++ src/moderator/js/setup.js
@@ -25,19 +25,23 @@ mod.setup = {
     mod.assignments.get_my_assignments({
       callback: function () {
         console.log(mod.DATA.assignments);
-        // // render html
-        // mod.utils.render_template({
-        //   template: ,
-        //   target: ,
-        //   context: {assignments: mod.DATA.assignments}
-        // });
+
+        // prep our assignments context
+        var assignments = mod.DATA.assignments.filter(function (val, i, arr) {
+          val.title = val.questions[0].question_text;
+          val.expire_datetime = moment(val.expire_datetime).format('MMM Do YYYY');
+          val.url = 'view-assignment.html#'+val.assignment_id;
+          return true;
+        })
+
+        // render html
+        mod.utils.render_template({
+          template: '#my-assignment-li',
+          target: '.my-assignments-list',
+          context: {assignments: assignments}
+        });
       }
     });
-
-    // var packery = new Packery('.album-cover-grid', {
-    //   itemSelector: '.album-cover-gi',
-    //   gutter: 10
-    // });
 
   },
 

b78e4e7a379a69f2916a4dc9819a3d474fab28d4
luis <lrosario101@gmail.com>
2014-09-25 17:01:47 -0400

    wut.

--- src/app/js/init.js
+++ src/app/js/init.js
@@ -13,19 +13,25 @@ var DEBUG = false;
 
 window.onload = function() {
 
+  alert('WHAT');
+
   // if (DEBUG) localStorage.removeItem('yellr');
   localStorage.removeItem('yellr');
 
+  alert('removed localStorage');
+
   // check for pre-existing data, if none, create it
   if (localStorage.getItem('yellr') === null) {
+    alert('creating user');
     yellr.utils.create_user();
+    alert('save');
     yellr.utils.save();
   } else {
     // we ave existing local storage, load it
     yellr.utils.load_localStorage();
   }
 
-
+  alert('checking client/server version');
   // check version info
   yellr.utils.check_version();
 
